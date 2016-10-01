11f39a9e4f569e24db1361ff440fa73e34aad8be
Christopher Wells <cwellsny@nycap.rr.com>
2016-09-30 21:09:40 -0400

    Add initial boxes parsing
    Add a function for parsing the initial boxes of food.

--- main.cc
+++ main.cc
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+int* getInitialBoxes();
+
 int main(int argc, char *argv[]) {
     cout << "Hello, World!\n";
+    int *boxes = getInitialBoxes();
+    delete [] boxes;
+}
+
+int* getInitialBoxes() {
+    int *boxes = new int[4];
+    int nextBox = 0;
+    for(int i = 0; i < 4; i++) {
+        string foodType;
+        int boxAmount;
+        cin >> foodType >> boxAmount;
+        boxes[nextBox] = boxAmount;
+    }
+    return boxes;
 }
