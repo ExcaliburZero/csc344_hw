8e1f3b4d4f144cf113711a9f8cd4a17d0ab1c40c
Christopher Wells <cwellsny@nycap.rr.com>
2016-10-29 17:21:04 -0400

    Start implementing position pressing
    Start working on implementing position pressing.

--- board.pl
+++ board.pl
@@ -1,14 +1,23 @@
 :- module(board, [
-        createBoard/4, printBoard/1
+        createBoard/5, printBoard/1, pressPoint/4
+
+        , checkForMine/4
     ]).
 
-createBoard(number(Height), number(Width), number(Mines), Board) :-
-  Board = [
-        ['.', '.', 'X'],
-        ['.', 'X', '.'],
-        ['X', '.', '.']
+createBoard(number(Height), number(Width), number(Mines), MineBoard, VisualBoard) :-
+  MineBoard = [
+        [".", ".", "X"],
+        [".", "X", "."],
+        ["X", ".", "."]
+    ],
+  VisualBoard = [
+        ["#", "#", "#"],
+        ["#", "#", "#"],
+        ["#", "#", "#"]
     ]
   .
+
+% Board Printing
 
 printBoard(Board) :-
   printRows(Board)
@@ -28,4 +37,40 @@ printSingleRow([Head|Tail]) :-
   write(Head),
   write(' '),
   printSingleRow(Tail)
+  .
+
+% Point Pressing
+
+pressPoint(MineBoard, VisualBoard, X, Y) :-
+  write('pressed':X:Y), nl,
+  checkForMine(MineBoard, X, Y, IsMine)
+  .
+
+% checkForMine(+MineBoard,
+%              +X, +Y, -IsMine)
+%
+% Checks if the given position on the given board is a mine or not. Sets IsMine
+% to 0 if it is a mine, or a non-zero value if it is not a mine.
+checkForMine(MineBoard, X, Y, IsMine) :-
+  getElem(MineBoard, X, Row),
+  getElem(Row, Y, Element),
+  IsMine is Element - "X",
+  write(IsMine), nl
+  .
+
+% getElem(+List,
+%         +Index, -Element)
+%
+% Gets the element in the given list at the given index.
+getElem([], _, Element) :-
+  Element = []
+  .
+
+getElem([Head|_], 0, Element) :-
+  Element = Head
+  .
+
+getElem([_|Tail], Index, Element) :-
+  Index2 is Index - 1,
+  getElem(Tail, Index2, Element)
   .
--- minesweeper.pl
+++ minesweeper.pl
@@ -6,7 +6,14 @@
 :- initialization play.
 
 play :-
-  createBoard(number(1), number(2), number(3), Board),
-  printBoard(Board),
+  createBoard(number(1), number(2), number(3), MineBoard, VisualBoard),
+  printBoard(VisualBoard),
+  promptPress(MineBoard, VisualBoard),
   halt
+  .
+
+promptPress(MineBoard, VisualBoard) :-
+  write('>'), nl,
+  pressPoint(MineBoard, VisualBoard, 1, 2),
+  printBoard(VisualBoard)
   .
