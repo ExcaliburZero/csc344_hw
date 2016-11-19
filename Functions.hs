1ad4e6da22a7f711ac776c0533b5cfbb182fd235
Christopher Wells <cwellsny@nycap.rr.com>
2016-11-18 21:28:26 -0500

    Implement replace
    Implement the replace function.

--- Functions.hs
+++ Functions.hs
@@ -1,5 +1,7 @@
 module Functions where
 
+import Data.List (splitAt)
+
 -- | Applies the given function three times to the given value.
 --
 -- >>> irepeat (+1) 0
@@ -29,3 +31,23 @@ dup _ [] = []
 dup c (x:xs)
   | c == x    = c : c : dup c xs
   | otherwise = x :     dup c xs
+
+-- | Replaces items i through j with a replacement list.
+--
+-- >>> replace [1,2,3] (4,5) [100,200,300,400,500,600,700]
+-- [100,200,300,400,1,2,3,700]
+--
+-- >>> replace "big moneyed interests" (32,37) "a vote for me is a vote for the people"
+-- "a vote for me is a vote for the big moneyed interests"
+--
+-- >>> replace [Just 1, Just 2] (2, 4) [Nothing, Nothing, Nothing, Nothing, Nothing]
+-- [Nothing,Nothing,Just 1,Just 2]
+--
+-- >>> replace [1,2,3] (4,5) []
+-- []
+replace :: [a] -> (Int, Int) -> [a] -> [a]
+replace _ _      []   = []
+replace r (b, e) list = start ++ r ++ end
+  where
+    (_    , end) = splitAt (e + 1) list
+    (start, _  ) = splitAt b       list
